7031350d1756c60ca3175e1fb5842bcdea6e5c60
shinigami5253 <deadreaper5253@protonmail.com>
2024-02-19 11:49:22 +0100

    ajouts fichier

---
+++ motd-api.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+# Récupérer le message à retourner depuis la variable d'environnement MESSAGE
+message = os.environ.get('MESSAGE', 'Bonjour, bienvenue sur notre service MOTD!')
+
+# Récupérer le port d'écoute depuis la variable d'environnement APP_PORT
+port = int(os.environ.get('APP_PORT', 5000))  # Port par défaut : 5000
+
+@app.route('/')
+def get_motd():
+    return jsonify({'message': message})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=port)
